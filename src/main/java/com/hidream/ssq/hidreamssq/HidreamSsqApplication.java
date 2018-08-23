d01ec245fdc1fb59e86e824bde6c20c793da75f0
JoannL <liuxiao5@msn.com>
2018-08-23 19:27:54 +0800

    Add files via upload

---
+++ src/main/java/com/hidream/ssq/hidreamssq/HidreamSsqApplication.java
@@ -0,0 +1,12 @@
+package com.hidream.ssq.hidreamssq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidreamSsqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidreamSsqApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hidream/ssq/hidreamssq/HidreamSsqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hidream.ssq.hidreamssq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HidreamSsqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
